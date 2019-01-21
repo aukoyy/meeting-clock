752907c276ac67375f3613a2689380262c18d72b
Øyvind Aukner <oyvind.auk@gmail.com>
2019-01-21 22:06:29 +0100

    Fixing redux logic for toggle timer. now actually toggles.

--- src/actions/timerActions.js
+++ src/actions/timerActions.js
@@ -4,11 +4,7 @@ import store from '../store';
 
 export const toggleTimer = () => ({
     type: TOGGLE_TIMER,
-    // FIXME: Fix ! so toggle works both ways
-    // "Du skal aldri bruke getState()..."
-    // Er det her man skal bruke dispatch??
-    payload: !store.getState().timerShouldRun,
-  })
+})
 
 
 export const incrementTimer = () => ({
--- src/reducers/timerReducer.js
+++ src/reducers/timerReducer.js
@@ -11,7 +11,7 @@ export default function(state = initialState, action) {
         case TOGGLE_TIMER:
             return {
                 ...state,
-                timerShouldRun: action.payload
+                timerShouldRun: !state.timerShouldRun
             } 
         default:
             return state
