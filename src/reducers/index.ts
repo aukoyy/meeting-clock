04b52802795dfe73f0fe8bcb7d5ec012ea0686a7
Øyvind Aukner <oyvind.auk@gmail.com>
2019-01-22 09:35:38 +0100

    starting on interfaces

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -3,7 +3,12 @@ import { combineReducers } from 'redux'
 import salayReducer from './salaryReducer';
 import timerReducer from './timerReducer';
 
-export default combineReducers({
+export interface AppState {
+    salaries: SalaryState;
+    timer: TimerState;
+}
+
+export default combineReducers<AppState>({
     salaries: salayReducer,
     timer: timerReducer,
 });
