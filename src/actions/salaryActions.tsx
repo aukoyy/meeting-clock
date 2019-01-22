c935295bd91588dfca6b1651a70a3fb1cec05b4f
Øyvind Aukner <oyvind.auk@gmail.com>
2019-01-22 09:54:43 +0100

    More types. App is crashing

--- src/actions/salaryActions.tsx
+++ src/actions/salaryActions.tsx
@@ -1,11 +1,11 @@
 import { NEW_SALARY, UPDATE_SALARYSUM } from './types';
 
-export const addSalary = amount => ({
+export const addSalary = (amount: number) => ({
   type: NEW_SALARY,
   payload: amount,
 })
 
-export const updateSalarySum = (salarySum) => ({
+export const updateSalarySum = (salarySum: number) => ({
   type: UPDATE_SALARYSUM,
   payload: salarySum,
 })
