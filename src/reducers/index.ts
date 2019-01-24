4c9347b54610f6e0f2e3e03df7d2804a2c3e1ad6
Øyvind Aukner <oyvind.auk@gmail.com>
2019-01-24 07:49:00 +0100

    Changing react-scripts to react-scripts-ts
    It now runs in the terminal, but throws error: "Cannot find namespace
    'JSX'. The error seams to come from react-components...

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -3,12 +3,13 @@ import { combineReducers } from 'redux'
 import salayReducer from './salaryReducer';
 import timerReducer from './timerReducer';
 
-export interface AppState {
+/* export interface AppState {
     salaries: SalaryState;
     timer: TimerState;
-}
+} */
 
-export default combineReducers<AppState>({
+// export default combineReducers<AppState>({
+export default combineReducers({
     salaries: salayReducer,
     timer: timerReducer,
 });
