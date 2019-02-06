ae9bd72d19537e6db46354ba018047e7135e0fae
Øyvind Aukner <oyvind.auk@gmail.com>
2019-02-06 07:41:57 +0100

    comment out redux tools.

--- src/store.ts
+++ src/store.ts
@@ -10,10 +10,11 @@ const middleware = [thunk];
 const store = createStore(
     rootReducer,
     initialState,
-    compose(
+    applyMiddleware(...middleware),
+    /* compose(
         applyMiddleware(...middleware),
-        (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-)
+        (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__() 
+    )*/
 );
 
 export default store;
